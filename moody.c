ed41c58cb76603952a939363a5c278ed9966d725
leanghok120 <leanghokoeng5@gmail.com>
2024-06-22 04:37:49 +0700

    Mapping windows and reparenting

--- moody.c
+++ moody.c
@@ -3,6 +3,22 @@
 #include <err.h>
 #include <stdio.h>
 
+void OnConfigureRequest(Display * dpy, const XConfigureRequestEvent ev) {
+  XWindowChanges changes;
+  changes.x = ev.x;
+  changes.y = ev.y;
+  changes.width = ev.width;
+  changes.height = ev.height;
+  changes.border_width = ev.border_width;
+  changes.sibling = ev.above;
+  changes.stack_mode = ev.detail;
+  XConfigureWindow(dpy, ev.window, ev.value_mask, &changes);
+}
+
+void OnMapRequest(Display * dpy, const XMapRequestEvent ev) {
+  XMapWindow(dpy, ev.window);
+}
+
 int main() {
   Display * dpy;
   Window root;
@@ -26,11 +42,17 @@ int main() {
       case CreateNotify: 
         printf("CreateNotify event: Window ID: %ld\n", ev.xcreatewindow.window);
         break;
+      case ConfigureRequest:
+        OnConfigureRequest(dpy, ev.xconfigurerequest);
+        break;
       case DestroyNotify:
         printf("DestroyNotify event: Window ID: %ld\n", ev.xdestroywindow.window);
         break;
       case ReparentNotify:
         printf("ReparentNotify event: Window ID: %ld\n", ev.xreparent.window);
+        break;
+      case MapRequest:
+        OnMapRequest(dpy, ev.xmaprequest);
         break;
     default:
       printf("Ignore event\n");

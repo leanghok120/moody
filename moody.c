44cde91fe9291313e31eb5cdb34e9430217a8890
leanghok120 <leanghokoeng5@gmail.com>
2024-06-22 20:09:37 +0700

    Add frames to windows (Not working)

--- moody.c
+++ moody.c
@@ -3,6 +3,31 @@
 #include <err.h>
 #include <stdio.h>
 
+void Frame(Display * dpy, Window root, Window w) {
+  // Visual stuff for frame
+  const unsigned int BORDER_WITDH = 3;
+  const unsigned long BORDER_COLOR = 0xffffff;
+  const unsigned long BG_COLOR = 0x000000;
+
+  // Get window attributes
+  XWindowAttributes x_win_attrs;
+  XGetWindowAttributes(dpy, w, &x_win_attrs);
+
+  // Create frame
+  const Window frame = XCreateSimpleWindow(dpy, root, x_win_attrs.x, x_win_attrs.y, x_win_attrs.width, x_win_attrs.height, BORDER_WITDH, BORDER_COLOR, BG_COLOR);
+
+  // Select events on frame
+  XSelectInput(dpy, frame, SubstructureRedirectMask | SubstructureNotifyMask);
+  XAddToSaveSet(dpy, w);
+
+  // Reparent client windows
+  XReparentWindow(dpy, w, frame, 0, 0);
+
+  XMapWindow(dpy, frame);
+
+  printf("Framed window");
+}
+
 void OnConfigureRequest(Display * dpy, const XConfigureRequestEvent ev) {
   XWindowChanges changes;
   changes.x = ev.x;
@@ -13,9 +38,11 @@ void OnConfigureRequest(Display * dpy, const XConfigureRequestEvent ev) {
   changes.sibling = ev.above;
   changes.stack_mode = ev.detail;
   XConfigureWindow(dpy, ev.window, ev.value_mask, &changes);
+  printf("Resize window");
 }
 
-void OnMapRequest(Display * dpy, const XMapRequestEvent ev) {
+void OnMapRequest(Display * dpy, Window root, const XMapRequestEvent ev) {
+  Frame(dpy, root, ev.window);
   XMapWindow(dpy, ev.window);
 }
 
@@ -39,7 +66,7 @@ int main() {
     printf("Recieved an event\n");
 
     switch (ev.type) {
-      case CreateNotify: 
+      case CreateNotify:
         printf("CreateNotify event: Window ID: %ld\n", ev.xcreatewindow.window);
         break;
       case ConfigureRequest:
@@ -52,10 +79,10 @@ int main() {
         printf("ReparentNotify event: Window ID: %ld\n", ev.xreparent.window);
         break;
       case MapRequest:
-        OnMapRequest(dpy, ev.xmaprequest);
+        OnMapRequest(dpy, root, ev.xmaprequest);
         break;
     default:
-      printf("Ignore event\n");
+      printf("Ignore event: %d\n", ev.type);
     }
   }
 

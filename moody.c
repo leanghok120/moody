d18f2bb3be626769fefe16409af7781a66626cc2
leanghok120 <leanghokoeng5@gmail.com>
2024-06-25 16:26:44 +0700

    Add run() function

--- moody.c
+++ moody.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Function to handle MapRequest events
 void handle_map_request(XEvent ev, Display *dpy, int scr) {
     XWindowAttributes attr;
     XGetWindowAttributes(dpy, ev.xmaprequest.window, &attr);
@@ -29,25 +28,8 @@ void handle_map_request(XEvent ev, Display *dpy, int scr) {
     XMapWindow(dpy, ev.xmaprequest.window);
 }
 
-int main() {
-    Display *dpy;
-    int scr;
-    Window root;
-    XEvent ev;
-
-    dpy = XOpenDisplay(NULL);
-    if (dpy == NULL) {
-        errx(1, "Couldn't open display");
-    }
-
-    scr = DefaultScreen(dpy);
-    root = RootWindow(dpy, scr);
-
-    // Select events to listen for on the root window
-    XSelectInput(dpy, root, SubstructureRedirectMask | SubstructureNotifyMask | KeyPressMask);
-
+void run(Display * dpy, XEvent ev, int scr) {
     printf("Event loop started\n");
-
     for (;;) {
         XNextEvent(dpy, &ev);
 
@@ -66,6 +48,26 @@ int main() {
                 break;
         }
     }
+}
+
+int main() {
+    Display *dpy;
+    int scr;
+    Window root;
+    XEvent ev;
+
+    dpy = XOpenDisplay(NULL);
+    if (dpy == NULL) {
+        errx(1, "Couldn't open display");
+    }
+
+    scr = DefaultScreen(dpy);
+    root = RootWindow(dpy, scr);
+
+    // Select events to listen for on the root window
+    XSelectInput(dpy, root, SubstructureRedirectMask | SubstructureNotifyMask | KeyPressMask);
+
+    run(dpy, ev, scr);
 
     XCloseDisplay(dpy);
     return 0;
